11876bbf15f89a6b7c0e0b6eb7e9508943c03b17
Siarhei Zabashnyj <demcrit@mail.ru>
2017-08-25 11:45:25 +0300

    last patch

--- Test/src/by/home/fibonacci/Main.java
+++ Test/src/by/home/fibonacci/Main.java
@@ -1,23 +1,20 @@
 package by.home.fibonacci;
-
 import java.util.Scanner;
-
 public class Main {
-	public static void main(String[] args) {
-	int n = 0;
-	System.out.println("Числа Фибоначчи в диапазоне от 1 до N");
-	System.out.print("Введите N:");
-	Scanner sc = new Scanner(System.in);
-	n = sc.nextInt();
-	int a = 1, b = 1;
+ public static void main(String[] args) {
+ int n = 0;
+ System.out.println("Числа Фибоначчи в диапазоне от 1 до N");
+ System.out.print("Введите N:");
+ Scanner sc = new Scanner(System.in);
+ n = sc.nextInt();
+ int a = 1, b = 1;
     System.out.print(a + " " + b);
-    int num = 2, i = 2;
-    while (num >= n) {
-        num = a + b;
-        a = b;
-        b = num;
-        System.out.print(" " + num);
-        i++;
+    int num = a+b;
+    while (num < n) {
+     a = b;
+     b = num;
+     System.out.print(" " + num);
+     num = a + b;
     }
 }
 }
